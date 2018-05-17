e8d5dc58941956a6b4748e72576e9b166ba8ddbd
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2018-05-17 16:33:30 +0200

    fixed some bugs on vao and index buffer

--- FastDrawing/Source/Example.cpp
+++ FastDrawing/Source/Example.cpp
@@ -10,21 +10,17 @@ int main(int argc, char* args[])
 
 
 	std::vector<Vertex> vertices;
-	Vertex v1 = { -0.5f, -0.5f, 0.0f,		0.0f,0.0f };
-	Vertex v2 = { 0.5f, -0.5f, 0.0f,		1.0f,0.0f};
-	Vertex v3 = { 0.5f, 0.5f, 0.0f,			1.1f,1.1f};
-
-	Vertex v4 = { -0.5f, -0.5f, 0.0f,		0.0f,0.0f };
-	Vertex v5 = { 0.5f, 0.5f, 0.0f,			1.0f, 1.0f};
-	Vertex v6 = { -0.5f, 0.5f, 0.0f,		0.0f,1.0f};
+	Vertex v1 = { 0.5f, 0.5f, 0.0f,			1.0f,0.0f };
+	Vertex v2 = { 0.5f, -0.5f, 0.0f,		1.0f,1.0f};
+	Vertex v3 = { -0.5f, -0.5f, 0.0f,		0.0f,1.0f};
+	Vertex v4 = { -0.5f,  0.5f, 0.0f,		0.0f, 0.0f };
+			
 
 	vertices.push_back(v1);
 	vertices.push_back(v2);
 	vertices.push_back(v3);
-
 	vertices.push_back(v4);
-	vertices.push_back(v5);
-	vertices.push_back(v6);
+	
 
 	std::string vertex = "#version 330 core\n"
 		"layout(location = 0) in vec3 aPos;\n"
